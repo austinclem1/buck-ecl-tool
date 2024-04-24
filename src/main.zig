0c82fcec91a783e967c401251b0d15c10213ef37
Austin Clements <austin.clements@smoothstack.com>
2024-04-24 10:52:40 -0500

    use buffered writer for stderr

--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,8 @@ const ecl_base: u16 = 0x6af6;
 const memdump_path = "salvation.dmp";
 
 pub fn main() !void {
+    var stderr = std.io.bufferedWriter(std.io.getStdErr().writer());
+
     var gpa = GPA{};
     defer _ = gpa.deinit();
     var allocator = gpa.allocator();
@@ -29,7 +31,7 @@ pub fn main() !void {
         // We can't just remove it from the list of level_ids because each id's index in the array is
         // significant to finding the rom address of the compressed level data
         if (level_id == 0x61) continue;
-        std.debug.print("\n\n\nlevel id {x}\n", .{level_id});
+        try stderr.writer().print("\n\n\nlevel id {x}\n", .{level_id});
 
         const rom_file = try std.fs.cwd().openFile("buck.md", .{});
         defer rom_file.close();
@@ -40,48 +42,51 @@ pub fn main() !void {
         var decoder = try LzwDecoder.init(allocator);
         defer decoder.deinit();
 
-        std.debug.print("decompressing script from {x}\n", .{compressed_script_addr});
+        try stderr.writer().print("decompressing script from {x}\n", .{compressed_script_addr});
         try rom_file.seekTo(compressed_script_addr);
         const script = try decoder.decompressAlloc(allocator, rom_file.reader());
         defer allocator.free(script);
 
         decoder.resetRetainingCapacity();
 
-        std.debug.print("decompressing text from {x}\n", .{compressed_text_addr});
+        try stderr.writer().print("decompressing text from {x}\n", .{compressed_text_addr});
         try rom_file.seekTo(compressed_text_addr);
         const text = try decoder.decompressAlloc(allocator, rom_file.reader());
         defer allocator.free(text);
 
         const text_base: u16 = @intCast(ecl_base + script.len);
 
-        std.debug.print("script: {x} - {x}\n", .{ ecl_base, ecl_base + script.len });
-        std.debug.print("text: {x} - {x}\n", .{ text_base, text_base + text.len });
+        try stderr.writer().print("script: {x} - {x}\n", .{ ecl_base, ecl_base + script.len });
+        try stderr.writer().print("text: {x} - {x}\n", .{ text_base, text_base + text.len });
 
         var parser = try CommandParser.init(allocator, script, text);
         defer parser.deinit();
 
         try parser.parseEcl();
 
-        std.debug.print("\n", .{});
+        try stderr.writer().print("\n", .{});
         for (parser.strings.items) |s| {
-            std.debug.print("str_{x}=\"{s}\"\n", .{ s.offset, s.bytes });
+            try stderr.writer().print("str_{x}=\"{s}\"\n", .{ s.offset, s.bytes });
             std.debug.assert(std.mem.indexOfScalar(u8, s.bytes, '"') == null);
+            std.debug.assert(std.mem.indexOfScalar(u8, s.bytes, '\n') == null);
         }
-        std.debug.print("\n", .{});
+        try stderr.writer().print("\n", .{});
 
         parser.sortLabelsByAddress();
         parser.sortVarsByAddress();
 
         try parser.ensureStringArgsAccountedFor();
-        if (parser.initialized_bytes) |bytes| {
-            std.debug.print("\ninitialized bytes: {s}\n\n", .{std.fmt.fmtSliceHexLower(bytes)});
+        if (parser.initialized_data) |data| {
+            const end_address = data.address + data.bytes.len;
+            try stderr.writer().print("\ninitialized bytes {x}-{x}\n", .{ data.address, end_address });
+            try stderr.writer().print("{s}\n\n", .{std.fmt.fmtSliceHexLower(data.bytes)});
         }
 
         var it = parser.vars.iterator();
         while (it.next()) |e| {
-            std.debug.print("{s} {x}\n", .{ @tagName(e.value_ptr.*), e.key_ptr.* });
+            try stderr.writer().print("{s} {x}\n", .{ @tagName(e.value_ptr.*), e.key_ptr.* });
         }
-        std.debug.print("\n", .{});
+        try stderr.writer().print("\n", .{});
 
         parser.detectVariableAliasing();
 
@@ -90,18 +95,20 @@ pub fn main() !void {
         for (parser.commands.items) |command| {
             if (next_label) |label| {
                 if (command.address == label.key_ptr.*) {
-                    std.debug.print("LABEL_{x}:\n", .{label.key_ptr.*});
+                    try stderr.writer().print("LABEL_{x}:\n", .{label.key_ptr.*});
                     next_label = labels_it.next();
                 }
             }
-            std.debug.print("    {s}", .{@tagName(command.tag)});
+            try stderr.writer().print("    {s}", .{@tagName(command.tag)});
             for (parser.getCommandArgs(command)) |arg| {
-                std.debug.print(" ", .{});
-                try arg.writeString(std.io.getStdErr().writer());
+                try stderr.writer().print(" ", .{});
+                try arg.writeString(stderr.writer());
             }
-            std.debug.print("\n", .{});
+            try stderr.writer().print("\n", .{});
         }
     }
+
+    try stderr.flush();
 }
 
 const level_ids = [_]u8{ 0x00, 0x01, 0x03, 0x10, 0x11, 0x20, 0x21, 0x22, 0x23, 0x30, 0x31, 0x32, 0x34, 0x40, 0x41, 0x42, 0x43, 0x50, 0x51, 0x52, 0x53, 0x5e, 0x5f, 0x60, 0x61, 0x62, 0x63 };
