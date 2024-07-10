4568f6266bf223e8927c366e8af7bb5317e1a420
Austin Clements <austin.clements@smoothstack.com>
2024-07-10 10:12:04 -0500

    print vars when serializing to text

--- src/ecl/Ast.zig
+++ src/ecl/Ast.zig
@@ -71,6 +71,19 @@ pub fn serializeText(self: *const @This(), writer: anytype) !void {
         try writer.print("\t{s}\n", .{self.blocks[block_index].label});
     }
 
+    try writer.print("\n", .{});
+    for (self.vars) |v| {
+        switch (v.var_type) {
+            .byte, .word, .dword => {
+                try writer.print("{s}: {s} @ 0x{x}\n", .{ v.name, @tagName(v.var_type), v.address });
+            },
+            .pointer => {
+                try writer.print("{s}: pointer = 0x{x}\n", .{ v.name, v.address });
+            },
+        }
+    }
+    try writer.print("\n", .{});
+
     for (self.blocks) |block| {
         try writer.print("{s}:\n", .{block.label});
         for (self.getBlockCommands(block)) |cmd| {
