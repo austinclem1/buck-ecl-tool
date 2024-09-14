889b488764c6b5f0ac1edc0bd2c2c25543137b30
Austin Clements <austin.clements@smoothstack.com>
2024-09-14 09:30:54 -0500

    update rom seeking functions, try new example for patching rom

--- src/ecl.zig
+++ src/ecl.zig
@@ -2,28 +2,78 @@ const std = @import("std");
 
 const File = std.fs.File;
 
+pub const Ast = @import("ecl/Ast.zig");
+
 pub const binary_parser = @import("ecl/binary_parser.zig");
 pub const TextParser = @import("ecl/TextParser.zig");
 
 pub const level_ids = [_]u8{ 0x00, 0x01, 0x03, 0x10, 0x11, 0x20, 0x21, 0x22, 0x23, 0x30, 0x31, 0x32, 0x34, 0x40, 0x41, 0x42, 0x43, 0x50, 0x51, 0x52, 0x53, 0x5e, 0x5f, 0x60, 0x61, 0x62, 0x63 };
 
-const compressedScriptsTableAddr = 0x38d02;
-const compressedTextTableAddr = 0x42b2a;
+const script_offsets_table_addr = 0x38d02;
+const scripts_section_end_addr = 0x42b0a;
 
-pub fn getCompressedScriptAddress(rom_file: File, id: u8) !u32 {
-    const level_index = std.mem.indexOfScalar(u8, &level_ids, id) orelse return error.LevelIdNotFound;
+const text_offsets_table_addr = 0x42b2a;
+const text_section_end_addr = 0x51102;
 
-    try rom_file.seekTo(compressedScriptsTableAddr + level_index * 4);
-    const offset = try rom_file.reader().readInt(u32, .big);
+pub fn fileReadCompressedScriptAlloc(allocator: std.mem.Allocator, rom_file: File, level_id: u8) ![]const u8 {
+    const start_addr, const end_addr = try getScriptAddrs(rom_file, level_id);
 
-    return compressedScriptsTableAddr + offset;
+    const script_len = end_addr - start_addr;
+    const buffer = try allocator.alloc(u8, script_len);
+    errdefer allocator.free(buffer);
+
+    try rom_file.seekTo(start_addr);
+    _ = try rom_file.readAll(buffer);
+
+    return buffer;
 }
 
-pub fn getCompressedTextAddress(rom_file: File, id: u8) !u32 {
-    const level_index = std.mem.indexOfScalar(u8, &level_ids, id) orelse return error.LevelIdNotFound;
+pub fn getScriptAddrs(rom_file: File, level_id: u8) !struct { u32, u32 } {
+    const level_index = std.mem.indexOfScalar(u8, &level_ids, level_id) orelse return error.LevelIdNotFound;
 
-    try rom_file.seekTo(compressedTextTableAddr + level_index * 4);
-    const offset = try rom_file.reader().readInt(u32, .big);
+    try rom_file.seekTo(script_offsets_table_addr + level_index * 4);
+    const start_addr = try rom_file.reader().readInt(u32, .big) + script_offsets_table_addr;
+    const end_addr = blk: {
+        if (level_index == level_ids.len - 1) {
+            break :blk scripts_section_end_addr;
+        } else {
+            const offset = try rom_file.reader().readInt(u32, .big);
+            break :blk script_offsets_table_addr + offset;
+        }
+    };
 
-    return compressedTextTableAddr + offset;
+    return .{ start_addr, end_addr };
+}
+
+pub fn fileReadCompressedTextAlloc(allocator: std.mem.Allocator, rom_file: File, level_id: u8) ![]const u8 {
+    const level_index = std.mem.indexOfScalar(u8, &level_ids, level_id) orelse return error.LevelIdNotFound;
+
+    try rom_file.seekTo(text_offsets_table_addr + level_index * 4);
+    const start_addr, const end_addr = try getTextAddrs(rom_file, level_id);
+
+    const text_len = end_addr - start_addr;
+    const buffer = try allocator.alloc(u8, text_len);
+    errdefer allocator.free(buffer);
+
+    try rom_file.seekTo(start_addr);
+    _ = try rom_file.readAll(buffer);
+
+    return buffer;
+}
+
+pub fn getTextAddrs(rom_file: File, level_id: u8) !struct { u32, u32 } {
+    const level_index = std.mem.indexOfScalar(u8, &level_ids, level_id) orelse return error.LevelIdNotFound;
+
+    try rom_file.seekTo(text_offsets_table_addr + level_index * 4);
+    const start_addr = try rom_file.reader().readInt(u32, .big) + text_offsets_table_addr;
+    const end_addr = blk: {
+        if (level_index == level_ids.len - 1) {
+            break :blk text_section_end_addr;
+        } else {
+            const offset = try rom_file.reader().readInt(u32, .big);
+            break :blk text_offsets_table_addr + offset;
+        }
+    };
+
+    return .{ start_addr, end_addr };
 }
--- src/main.zig
+++ src/main.zig
@@ -11,4 +11,85 @@ const ecl_base: u16 = 0x6af6;
 
 const tokenize = @import("ecl/tokenize.zig");
 
-pub fn main() !void {}
+const input_ecl_path = "new_16.ecl";
+const input_rom_path = "buck.md";
+const output_rom_path = "buck rodgers buc05.u1"; // crazy default name for MAME
+
+pub fn main() !void {
+    var gpa = GPA{};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    std.debug.print("Parsing \"{s}\"\n", .{input_ecl_path});
+    var ast = try parseFile(allocator, input_ecl_path);
+    defer ast.deinit();
+
+    const ecl_binary = try ast.serializeBinary(allocator);
+    defer allocator.free(ecl_binary.script);
+    defer allocator.free(ecl_binary.text);
+
+    std.debug.print("Reading rom data from \"{s}\"\n", .{input_rom_path});
+    const in_rom = try std.fs.cwd().openFile(input_rom_path, .{});
+    defer in_rom.close();
+
+    var rom_bytes = try in_rom.readToEndAlloc(allocator, 1024 * 1024 * 4);
+    defer allocator.free(rom_bytes);
+
+    {
+        const compressed_script = blk: {
+            var encoder = try lzw.Encoder.init(allocator);
+            defer encoder.deinit();
+            break :blk try encoder.compressAlloc(allocator, ecl_binary.script);
+        };
+        defer allocator.free(compressed_script);
+
+        const dest_start, const dest_end = try ecl.getScriptAddrs(in_rom, 0x10);
+        const max_script_size = dest_end - dest_start;
+        if (compressed_script.len > max_script_size) {
+            std.debug.print("resulting script too long: {d} (max {d})\n", .{ compressed_script.len, max_script_size });
+            return;
+        }
+
+        std.mem.copyForwards(u8, rom_bytes[dest_start..dest_end], compressed_script);
+    }
+
+    {
+        const compressed_text = blk: {
+            var encoder = try lzw.Encoder.init(allocator);
+            defer encoder.deinit();
+            break :blk try encoder.compressAlloc(allocator, ecl_binary.text);
+        };
+        defer allocator.free(compressed_text);
+
+        const dest_start, const dest_end = try ecl.getTextAddrs(in_rom, 0x10);
+        const max_text_size = dest_end - dest_start;
+
+        if (compressed_text.len > max_text_size) {
+            std.debug.print("resulting text too long: {d} (max {d})\n", .{ compressed_text.len, max_text_size });
+        }
+
+        std.mem.copyForwards(u8, rom_bytes[dest_start..dest_end], compressed_text);
+    }
+
+    // turn checksum check into NOP
+    rom_bytes[0xfffd0] = 0x4e;
+    rom_bytes[0xfffd1] = 0x71;
+
+    var out_rom = try std.fs.cwd().createFile(output_rom_path, .{});
+    defer out_rom.close();
+
+    try out_rom.writeAll(rom_bytes);
+}
+
+fn parseFile(allocator: std.mem.Allocator, path: []const u8) !ecl.Ast {
+    var file = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
+
+    const file_bytes = try file.readToEndAlloc(allocator, 1024 * 1024);
+    defer allocator.free(file_bytes);
+
+    var parser = ecl.TextParser.init(allocator);
+    defer parser.deinit();
+
+    return parser.parse(file_bytes);
+}
